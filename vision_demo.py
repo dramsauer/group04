0905335ab3bb05e0e5760a01b6a3bca532bd6b2b
dramsauer <dominik.ramsauer@stud.uni-regensburg.de>
2018-03-02 15:37:11 +0100

    init

--- vision_demo.py
+++ vision_demo.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import copy
 
+
 def threshold(img, threshold):
     "Thresholds an image"
     _i = 0
@@ -10,13 +11,13 @@ def threshold(img, threshold):
         _j = 0
         while _j < img.shape[1]:
             if (img.item(_i, _j, 0) + img.item(_i, _j, 1) + img.item(_i, _j, 2)) > threshold:
-                img[_i,_j][0] = 255
-                img[_i,_j][1] = 255
-                img[_i,_j][2] = 255
+                img[_i, _j][0] = 255
+                img[_i, _j][1] = 255
+                img[_i, _j][2] = 255
             else:
-                img[_i,_j][0] = 0
-                img[_i,_j][1] = 0
-                img[_i,_j][2] = 0
+                img[_i, _j][0] = 0
+                img[_i, _j][1] = 0
+                img[_i, _j][2] = 0
             _j += 1
         _i += 1
     print("Threshold done.")
@@ -31,15 +32,16 @@ def extractBlueRGB(img):
     while _i < img.shape[0]:
         _j = 0
         while _j < img.shape[1]:
-            #img[_i,_j][0] = 0
-            img[_i,_j][1] = 0
-            img[_i,_j][2] = 0
+            # img[_i,_j][0] = 0
+            img[_i, _j][1] = 0
+            img[_i, _j][2] = 0
             _j += 1
         _i += 1
 
     print("Channel extraction via RGB done.")
 
     return img
+
 
 def extractBlueHSV(img):
     "Extracts blue channel based on HSV"
@@ -48,8 +50,8 @@ def extractBlueHSV(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # define range of blue color in HSV
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
+    lower_blue = np.array([110, 50, 50])
+    upper_blue = np.array([130, 255, 255])
 
     # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
@@ -67,9 +69,9 @@ def thresholdOtsu(img):
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    th, img = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    th, img = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-    if img[0,0] == 0:
+    if img[0, 0] == 0:
         cv2.bitwise_not(img, img);
 
     img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
@@ -90,7 +92,7 @@ def searchtemplate(img, template):
     top_left = max_loc
     bottom_right = (top_left[0] + w, top_left[1] + h)
 
-    cv2.rectangle(img, top_left, bottom_right, (0,0,255), 10)
+    cv2.rectangle(img, top_left, bottom_right, (0, 0, 255), 10)
 
     print("Template search done.")
 
@@ -102,39 +104,39 @@ def regiongrowing(img):
     _i = 1
     _j = 1
     img1 = copy.deepcopy(img)
-    while _i < img.shape[0]-1:
+    while _i < img.shape[0] - 1:
         _j = 0
-        while _j < img.shape[1]-1:
+        while _j < img.shape[1] - 1:
             if (img.item(_i, _j, 0) == 0 and img.item(_i, _j, 1) == 0 and img.item(_i, _j, 2) == 0):
-                img1[_i-1, _j-1][0] = 0
-                img1[_i-1, _j-1][1] = 0
-                img1[_i-1, _j-1][2] = 0
-                img1[_i  , _j-1][0] = 0
-                img1[_i  , _j-1][1] = 0
-                img1[_i  , _j-1][2] = 0
-                img1[_i+1, _j-1][0] = 0
-                img1[_i+1, _j-1][1] = 0
-                img1[_i+1, _j-1][2] = 0
+                img1[_i - 1, _j - 1][0] = 0
+                img1[_i - 1, _j - 1][1] = 0
+                img1[_i - 1, _j - 1][2] = 0
+                img1[_i, _j - 1][0] = 0
+                img1[_i, _j - 1][1] = 0
+                img1[_i, _j - 1][2] = 0
+                img1[_i + 1, _j - 1][0] = 0
+                img1[_i + 1, _j - 1][1] = 0
+                img1[_i + 1, _j - 1][2] = 0
 
-                img1[_i-1, _j][0] = 0
-                img1[_i-1, _j][1] = 0
-                img1[_i-1, _j][2] = 0
-                img1[_i  , _j][0] = 0
-                img1[_i  , _j][1] = 0
-                img1[_i  , _j][2] = 0
-                img1[_i+1, _j][0] = 0
-                img1[_i+1, _j][1] = 0
-                img1[_i+1, _j][2] = 0
+                img1[_i - 1, _j][0] = 0
+                img1[_i - 1, _j][1] = 0
+                img1[_i - 1, _j][2] = 0
+                img1[_i, _j][0] = 0
+                img1[_i, _j][1] = 0
+                img1[_i, _j][2] = 0
+                img1[_i + 1, _j][0] = 0
+                img1[_i + 1, _j][1] = 0
+                img1[_i + 1, _j][2] = 0
 
-                img1[_i-1, _j+1][0] = 0
-                img1[_i-1, _j+1][1] = 0
-                img1[_i-1, _j+1][2] = 0
-                img1[_i  , _j+1][0] = 0
-                img1[_i  , _j+1][1] = 0
-                img1[_i  , _j+1][2] = 0
-                img1[_i+1, _j+1][0] = 0
-                img1[_i+1, _j+1][1] = 0
-                img1[_i+1, _j+1][2] = 0
+                img1[_i - 1, _j + 1][0] = 0
+                img1[_i - 1, _j + 1][1] = 0
+                img1[_i - 1, _j + 1][2] = 0
+                img1[_i, _j + 1][0] = 0
+                img1[_i, _j + 1][1] = 0
+                img1[_i, _j + 1][2] = 0
+                img1[_i + 1, _j + 1][0] = 0
+                img1[_i + 1, _j + 1][1] = 0
+                img1[_i + 1, _j + 1][2] = 0
             _j += 1
         _i += 1
 
@@ -145,8 +147,8 @@ def regiongrowing(img):
 def track(img):
     "Counts cluster"
     img1 = copy.deepcopy(img)
-    img1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(img1,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(img1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     connectivity = 24
     cv2.bitwise_not(thresh, thresh)
     output = cv2.connectedComponentsWithStats(thresh, connectivity, cv2.CV_32S)
@@ -179,21 +181,22 @@ def track(img):
                 img[_i, _j][1] = 0
                 img[_i, _j][2] = 0
             else:
-                img[_i, _j][0] = 255/num_labels*output[1][_i][_j]
-                img[_i, _j][1] = 255/num_labels*output[1][_i][_j]
-                img[_i, _j][2] = 255/num_labels*output[1][_i][_j]
+                img[_i, _j][0] = 255 / num_labels * output[1][_i][_j]
+                img[_i, _j][1] = 255 / num_labels * output[1][_i][_j]
+                img[_i, _j][2] = 255 / num_labels * output[1][_i][_j]
             _j += 1
         _i += 1
 
     x1 = stats[yesCluster[0], cv2.CC_STAT_LEFT]
-    x2 = stats[yesCluster[0], cv2.CC_STAT_LEFT] +  stats[yesCluster[0], cv2.CC_STAT_WIDTH]
+    x2 = stats[yesCluster[0], cv2.CC_STAT_LEFT] + stats[yesCluster[0], cv2.CC_STAT_WIDTH]
     y1 = stats[yesCluster[0], cv2.CC_STAT_TOP]
     y2 = stats[yesCluster[0], cv2.CC_STAT_TOP] + stats[yesCluster[0], cv2.CC_STAT_HEIGHT]
 
-    cv2.rectangle(img, (x1,y1), (x2,y2), (0,0,255), 10)
+    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 10)
 
     font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(img, "Position: (" + str(x1+(x2-x1)/2) + "," + str(y1+(y2-y1)/2) + ")", (10,730), font, 2, (0,0,0), 2, cv2.LINE_AA)
+    cv2.putText(img, "Position: (" + str(x1 + (x2 - x1) / 2) + "," + str(y1 + (y2 - y1) / 2) + ")", (10, 730), font, 2,
+                (0, 0, 0), 2, cv2.LINE_AA)
 
     print("Clustering done. Found " + str(len(yesCluster)) + " cluster.")
     return img
@@ -202,8 +205,8 @@ def track(img):
 def cluster(img):
     "Counts cluster"
     img1 = copy.deepcopy(img)
-    img1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(img1,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(img1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     connectivity = 24
     cv2.bitwise_not(thresh, thresh)
     output = cv2.connectedComponentsWithStats(thresh, connectivity, cv2.CV_32S)
@@ -264,21 +267,22 @@ def cluster(img):
                 img[_i, _j][1] = 128
                 img[_i, _j][2] = 128
             else:
-                img[_i, _j][0] = 255/num_labels*output[1][_i][_j]
-                img[_i, _j][1] = 255/num_labels*output[1][_i][_j]
-                img[_i, _j][2] = 255/num_labels*output[1][_i][_j]
+                img[_i, _j][0] = 255 / num_labels * output[1][_i][_j]
+                img[_i, _j][1] = 255 / num_labels * output[1][_i][_j]
+                img[_i, _j][2] = 255 / num_labels * output[1][_i][_j]
             _j += 1
         _i += 1
 
     font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(img, "Found " + str(len(yesCluster)) + " objects.",(10,730), font, 2, (0,0,0), 2, cv2.LINE_AA)
+    cv2.putText(img, "Found " + str(len(yesCluster)) + " objects.", (10, 730), font, 2, (0, 0, 0), 2, cv2.LINE_AA)
 
     print("Clustering done. Found " + str(len(yesCluster)) + " cluster.")
     return img
 
-img = cv2.imread('./demo_image.jpg',3)
-template = cv2.imread('./template.jpg',3)
-cv2.imshow('image',img)
+
+img = cv2.imread('./demo_image.jpg', 3)
+template = cv2.imread('./template.jpg', 3)
+cv2.imshow('image', img)
 k = cv2.waitKey(0)
 
 while (k != 27):
@@ -291,37 +295,37 @@ while (k != 27):
  6 Convert to gray and Otsu\'s Threshold\n\
  7 Connected Component Analysis and position of largest cluster\n\
  8 Template matching")
-    if k == ord('s'): # wait for 's' key to save and exit
-        cv2.imwrite('save.png',img)
+    if k == ord('s'):  # wait for 's' key to save and exit
+        cv2.imwrite('save.png', img)
         cv2.destroyAllWindows()
-    elif k == ord('0'): # original
+    elif k == ord('0'):  # original
         print("Loaded original file from disk.")
         img = cv2.imread('./demo_image.jpg', 3)
-        cv2.imshow('image',img)
-    elif k == ord('1'): # threshold
+        cv2.imshow('image', img)
+    elif k == ord('1'):  # threshold
         threshold(img, 440)
-        cv2.imshow('image',img)
-    elif k == ord('2'): # region growing
+        cv2.imshow('image', img)
+    elif k == ord('2'):  # region growing
         img = regiongrowing(img)
-        cv2.imshow('image',img)
-    elif k == ord('3'): # cluster
+        cv2.imshow('image', img)
+    elif k == ord('3'):  # cluster
         img = cluster(img)
-        cv2.imshow('image',img)
-    elif k == ord('4'): # extract blue channel via RGB
+        cv2.imshow('image', img)
+    elif k == ord('4'):  # extract blue channel via RGB
         img = extractBlueRGB(img)
-        cv2.imshow('image',img)
-    elif k == ord('5'): # extract blue channel via HSV
+        cv2.imshow('image', img)
+    elif k == ord('5'):  # extract blue channel via HSV
         img = extractBlueHSV(img)
-        cv2.imshow('image',img)
-    elif k == ord('6'): # Otsu's Threshold
+        cv2.imshow('image', img)
+    elif k == ord('6'):  # Otsu's Threshold
         img = thresholdOtsu(img)
-        cv2.imshow('image',img)
-    elif k == ord('7'): # Track largest cluster
+        cv2.imshow('image', img)
+    elif k == ord('7'):  # Track largest cluster
         img = track(img)
-        cv2.imshow('image',img)
-    elif k == ord('8'): # Search template
+        cv2.imshow('image', img)
+    elif k == ord('8'):  # Search template
         img = searchtemplate(img, template)
-        cv2.imshow('image',img)
+        cv2.imshow('image', img)
 
     k = cv2.waitKey(0)
 
